1f6178069ca4471456de68f8923a6114b25158a4
Loris Prataiolo <lorisprataiolo@outlook.com>
2024-03-05 16:21:01 +0100

    Update loginForm.php

--- php/loginForm.php
+++ php/loginForm.php
@@ -5,7 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
 </head>
+
+
 <body>
-    
+    <h1>Log In</h1>
+
+    <form action="access.php" method="post">
+        <div>
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email">
+        </div>
+
+        <div>
+            <label for="pass">password</label>
+            <input type="pass" id="pass" name="pass">
+        </div>
+
+        <input type="submit">
+   
+
+    </form>
 </body>
+
 </html>
