0c363af09f88a164567606e8bf32f54a7daf730d
kevinslashgun <kevinbarragan1402@gmail.com>
2024-03-05 15:58:57 +0100

    Inizio index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Autenticazione</title>
+</head>
+<body>
+    <form action="login.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="password" placeholder="Password">
+        <input type="submit" value="Login">
+    </form>
+    <a href="register.php">Registrati</a>
+    <a href="forgotPassword.php">Password dimenticata</a>
+    <a href="forgotUsername.php">Reset password</a>
+</body>
+</html>
