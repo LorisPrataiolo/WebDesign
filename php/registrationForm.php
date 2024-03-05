aae51ba5b4e9f2f55614315f63fcc67878c10384
Simo64Terminator <simo64.tomasella@gmail.com>
2024-03-05 16:18:06 +0100

    Changed registationForm.php

--- php/registrationForm.php
+++ php/registrationForm.php
@@ -6,6 +6,27 @@
     <title>Registration Page</title>
 </head>
 <body>
-    
+    <form id="form" action="registration.php">
+        
+        <label for="username">Username</label>
+        <input type="text" name="username" placeholder="Username">
+        
+        <label for="name">Name</label>
+        <input type="text" name="name" placeholder="Name">
+        
+        <label for="surname">Surname</label>
+        <input type="text" name="surname" placeholder="Surname">
+        
+        <label for="email">Email</label>
+        <input type="email" name="email" placeholder="Email">
+        
+        <label for="password">Password</label>
+        <input type="password" name="password" placeholder="Password">
+        
+        <label for="confirmPassword">Confirm Password</label>
+        <input type="password" name="confirmPassword" placeholder="Confirm Password">
+        
+        <input type="submit" value="Register">
+    </form> 
 </body>
 </html>
