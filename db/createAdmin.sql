8e0f516177655ac5056fec8341524f2b2d071439
kevinslashgun <kevinbarragan1402@gmail.com>
2024-03-05 17:58:04 +0100

    zioepra

--- db/createAdmin.sql
+++ db/createAdmin.sql
@@ -1,3 +1,3 @@
 DROP USER IF EXISTS 'webDesignAdmin';
 CREATE USER IF NOT EXISTS 'webDesignAdmin' IDENTIFIED BY 'admin';
-GRANT ALL PRIVILEGES ON webdesignDB. * TO 'webDesignAdmin';
+GRANT ALL PRIVILEGES ON webDesignDB. * TO 'webDesignAdmin';
